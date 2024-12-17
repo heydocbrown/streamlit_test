1dc174879d88715f57a70e162f6a3745cd78f892
heydocbrown <dbrownte@gmail.com>
2024-12-16 19:00:26 -0500

    simplified plot for readability

--- st_denali_test.py
+++ st_denali_test.py
@@ -16,89 +16,102 @@ def offset_value_per_cow(offset_price, mitigation_pct, baseline_CH4 = 300, GWP=2
     except:
         return 0
 
-
-offset_price_range = np.arange(101)
-mitigation_pct = st.number_input(
-    label = 'Methane Reduction (%)', value =25)
-
-baseline = st.number_input(
-    label='Baseline CH4 (g per cow daily)',value=300)
-interval = 'daily'
-
-interval = st.radio(label='Daily or Annual Value', options=['daily', 'annual'])
-
 gwp = 28
 
-milk_price = st.number_input('Milk price ($/liter)', 0.50)
-milk_production = st.number_input('Daily Milk produced (liter)', 4)
-boost = st.number_input('Productivity Boost from Intervention (%)', 0)
+with st.sidebar:    
+    xaxis= st.selectbox("X-Axis Variable", 
+        ['Methane Reduction (%)', "Carbon Offset Price"])
 
-if interval == 'daily':
-    added_value = boost*milk_price*milk_production*.01
-if interval == 'annual':
-    added_value =  boost*milk_price*milk_production*.01 * 365
-
-results = []
-
-for offset_p in offset_price_range:
-    offset_value = offset_value_per_cow(offset_p,
-                mitigation_pct, baseline, interval=interval, GWP=gwp)
-
-    results.append({
-        'Offset Price ($/t_CO2)' : offset_p,
-        '% Reduction': mitigation_pct,
-        'Baseline (g CH4 per day)':baseline,
-        'GWP': gwp,
-        'Interval': interval,
-        'Offset Value ($)': offset_value,
-        'Added Production Value ($)': added_value,
-        'Total Value ($)': added_value+offset_value,
-    })
+    if xaxis != 'Methane Reduction (%)':
+        mitigation_pct = st.number_input(
+            label = 'Methane Reduction (%)', value =25)
+    if xaxis != "Carbon Offset Price":
+        offset_price = st.number_input(
+            label = 'Price of Carbon ($/tCO2)', value=40)
+    baseline = st.number_input(
+        label='Baseline CH4 (g per cow daily)',value=300)
+    interval = st.radio(label='Daily or Annual Value', options=['daily', 'annual'])
+  
+    q_prod = st.checkbox("Productivity Increased?")
+    if q_prod == True:
+        milk_price = st.number_input('Milk price ($/liter)', 0.50)
+        milk_production = st.number_input('Daily Milk produced (liter)', 4)
+        boost = st.number_input('Productivity Boost from Intervention (%)', 0)
 
 
-df = pd.DataFrame(results)
-
-st.line_chart(data=df, x='Offset Price ($/t_CO2)',
-                     y='Offset Value ($)')
-
-
-st.line_chart(data=df, x='Offset Price ($/t_CO2)',
-                     y=['Offset Value ($)',
-                     'Added Production Value ($)',
-                     'Total Value ($)'])
+added_value =0
+if q_prod == True:
+    if interval == 'daily':
+        added_value = boost*milk_price*milk_production*.01
+    if interval == 'annual':
+        added_value =  boost*milk_price*milk_production*.01 * 365
 
 
 
+offset_price_range = np.arange(101)
 mitigation_pct_range = np.arange(101)
-offset_price = st.number_input(
-    label = 'Price of Carbon ($/tCO2)', value=40)
 
 
 results = []
+if xaxis=='Carbon Offset Price':
+    for offset_p in offset_price_range:
+        offset_value = offset_value_per_cow(offset_p,
+                    mitigation_pct, baseline, interval=interval, GWP=gwp)
 
-for mitigation_pct in mitigation_pct_range:
-    offset_value = offset_value_per_cow(offset_price,
-                mitigation_pct, baseline, interval=interval, GWP=gwp)
-
-    results.append({
-        'Offset Price ($/t_CO2)' : offset_price,
-        '% Reduction': mitigation_pct,
-        'Baseline (g CH4 per day)':baseline,
-        'GWP': gwp,
-        'Interval': interval,
-        'Offset Value ($)': offset_value,
-        'Added Production Value ($)': added_value,
-        'Total Value ($)': added_value+offset_value,
-    })
-
-
-df = pd.DataFrame(results)
-
-st.line_chart(data=df, x='% Reduction',
-                     y='Offset Value ($)')
+        results.append({
+            'Offset Price ($/t_CO2)' : offset_p,
+            'Methane Reduction (%)': mitigation_pct,
+            'Baseline (g CH4 per day)':baseline,
+            'GWP': gwp,
+            'Interval': interval,
+            'Offset Value ($)': offset_value,
+            'Added Production Value ($)': added_value,
+            'Total Value ($)': added_value+offset_value,
+        })
 
 
-st.line_chart(data=df, x='% Reduction',
-                     y=['Offset Value ($)',
-                     'Added Production Value ($)',
-                     'Total Value ($)'])
+    df = pd.DataFrame(results)
+
+    if q_prod != True:
+        st.line_chart(data=df, x='Offset Price ($/t_CO2)',
+                            y='Offset Value ($)')
+
+    if q_prod == True:
+        st.line_chart(data=df, x='Offset Price ($/t_CO2)',
+                            y=['Offset Value ($)',
+                            'Added Production Value ($)',
+                            'Total Value ($)'], y_label = 'Value ($)')
+
+
+
+
+
+results = []
+if xaxis == "Methane Reduction (%)":
+    for mitigation_pct in mitigation_pct_range:
+        offset_value = offset_value_per_cow(offset_price,
+                    mitigation_pct, baseline, interval=interval, GWP=gwp)
+
+        results.append({
+            'Offset Price ($/t_CO2)' : offset_price,
+            'Methane Reduction (%)': mitigation_pct,
+            'Baseline (g CH4 per day)':baseline,
+            'GWP': gwp,
+            'Interval': interval,
+            'Offset Value ($)': offset_value,
+            'Added Production Value ($)': added_value,
+            'Total Value ($)': added_value+offset_value,
+        })
+
+
+    df = pd.DataFrame(results)
+
+    if q_prod != True:
+        st.line_chart(data=df, x='Methane Reduction (%)',
+                            y='Offset Value ($)')
+
+    if q_prod == True:
+        st.line_chart(data=df, x='Methane Reduction (%)',
+                            y=['Offset Value ($)',
+                            'Added Production Value ($)',
+                            'Total Value ($)'], y_label = 'Value ($)')
